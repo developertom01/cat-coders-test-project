6d0c41cd76bdb67dd88b89f5d785a64c0455c9c0
Thomas Sarpong <53011231+developertom01@users.noreply.github.com>
2022-05-19 12:10:56 +0000

    Added database manager singleton

--- src/managers/DatabaseManager.ts
+++ src/managers/DatabaseManager.ts
@@ -4,10 +4,20 @@ export default class DatabaseManager {
   private static _instance: Sequelize;
   private static _initialize() {
     this._instance = new Sequelize({
-         dialect:"mariadb",
-         database:"dev",
-         host:process.env.DATABASE_HOST,
-         password:process.env.DATABASE_PASSWORD
+      dialect: "mariadb",
+      database: "dev",
+      host: process.env.DATABASE_HOST,
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USER,
     });
+  }
+  public static get instance() {
+    if (!this._instance) {
+      this._initialize();
+    }
+    return this._instance;
+  }
+  public static close() {
+    this._instance.close();
   }
 }
